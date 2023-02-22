38f14119de7d84d31a1ac630d08a1bf44f2e4aa8
rato <ulendarato@gmail.com>
2023-02-22 02:28:33 +0200

    fix 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,22 +2,39 @@
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
+ * @n: starting number to print from
+ *
+ * Return: void
  */
 void print_to_98(int n)
 {
-	while (n != 98)
+	int i;
+
+	if (n <= 98)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-		_putchar(',');
-		_putchar(' ');
-		if (n < 98)
-			n++;
-		else
-			n--;
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-		_putchar('9');
-		_putchar('8');
-		_putchar('\n');
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
